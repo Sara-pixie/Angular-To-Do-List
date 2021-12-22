1286ec2c4f65e9b6eb0d6c797bfe4ae8bec3c577
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-22 16:49:41 +0100

    App basic setup

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../../services/task.service";
+import {Task} from "../../Task";
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = []; 
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks;
+    });
+  }
+
+}
