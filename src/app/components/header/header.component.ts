43971c06130417bf46c018b668c052bfdecaae31
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-24 01:33:21 +0100

    Toggle functionality with Subject

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscribtion: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscribtion = this.uiService.onToggle().subscribe((value) =>{
+      this.showAddTask = value;
+    });
+   }
 
   ngOnInit(): void {
   }
   
   toggleAddTask(){
-    console.log("toggle");
+    this.uiService.toggleAddTask();
   }
 }
