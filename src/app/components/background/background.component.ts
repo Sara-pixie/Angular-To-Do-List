30c67248779187f616d4a395abd2bce925c5f020
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-29 00:09:04 +0100

    Animating background

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -17,8 +17,9 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
   colorArray:Array<string> = [
     "#FFF587", "#FF8C64", "#FF665A", "#7D6B7D", "#A3A1A8"
   ]
+  circleNumber: number = 500;
   //Creating an array of circles
-  circleArray: Array<Object> = [];
+  circleArray: Array<any> = [];
 
   constructor() { }
 
@@ -36,11 +37,9 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-
     this.context = this.canvasRef.nativeElement.getContext("2d")!;
-    
-    //store circles in array
-    for ( var i = 0; i < 500; i++){
+    //make circles
+    for ( var i = 0; i < this.circleNumber; i++){
     //set variables
       const minRadius = 3;
       const radius = Math.random() * 5 + minRadius;
@@ -50,18 +49,52 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
       const dx = (Math.random() - 0.5) * speed;
       const dy = (Math.random() - 0.5) * speed;
       const color = this.colorArray[Math.floor(Math.random() * this.colorArray.length)];
-      //make circle
-      //this.circleArray.push(new Circle(x, y, dx, dy, radius, speed, color));
-      this.context.beginPath();
+      //draw circle
+      this.init(x, y, dx, dy, radius, speed, color);
+    }
+    //console.log(this.circleArray);
+    this.animate();
+  }
+
+  private init(x: number, y: number, dx: number, dy: number, radius: number, speed: number, color: string){
+      const circle = {x, y, dx, dy, radius, speed, color};
+      this.circleArray.push(circle);
+    this.draw(x, y, dx, dy, radius, speed, color);
+  }
+
+  private draw(x: number, y: number, dx: number, dy: number, radius: number, speed: number, color: string){
+    this.context.beginPath();
       this.context.arc(x, y, radius, 0, Math.PI * 2, false);
       this.context.strokeStyle = color;
       this.context.stroke();
       this.context.fillStyle = color;
       this.context.fill();
-    }
   }
 
-  animate(): void {}
+  private update(circle: any){
+    //change directions when...
+    if (circle.x + circle.radius > this.innerWidth || circle.x - circle.radius < 0) {
+        circle.dx = -circle.dx;
+    }
+    if (circle.y + circle.radius > this.innerHeight || circle.y - circle.radius < 0) {
+        circle.dy = -circle.dy;
+    }
+    //make it move by changing x & y values for the next circle
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+    this.draw(circle.x, circle.y, circle.dx, circle.dy, circle.radius, circle.speed, circle.color);
+  }
+
+  private animate(){
+    //make a loop
+    requestAnimationFrame(() => this.animate());
+    //clear previous input
+    this.context.clearRect(0, 0, this.innerWidth, this.innerHeight);
+    //draw circles
+    for (var i = 0; i < this.circleArray.length; i++){
+        this.update(this.circleArray[i]);
+    }
+  }
 
   ngOnDestroy() {
     this.resizeSubscription$.unsubscribe()
